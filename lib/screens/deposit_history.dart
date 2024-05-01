3dece54778a8caf0ac9e5f071c529e7ea2ce260e
waqas8raza <rajputwaqass887@gmail.com>
2024-05-01 18:14:20 +0500

    deposit history page added

--- lib/screens/deposit_history.dart
+++ lib/screens/deposit_history.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
 import 'package:qafworld_app/screens/about_screen.dart';
 import 'package:qafworld_app/screens/bottom_navbar.dart';
 import 'package:qafworld_app/screens/contact_screen.dart';
@@ -7,6 +9,7 @@ import 'package:qafworld_app/screens/facts_screen.dart';
 import 'package:qafworld_app/screens/home.dart';
 import 'package:qafworld_app/screens/login_screen.dart';
 import 'package:qafworld_app/screens/payment_screen.dart';
+import 'package:qafworld_app/widgets/app_search_button.dart';
 import 'package:qafworld_app/widgets/text_field_widget.dart';
 
 class DepositHistoryScreen extends StatefulWidget {
@@ -30,10 +33,35 @@ class _DepositHistoryScreenState extends State<DepositHistoryScreen> {
   bool isOpen = false;
 
   List items = ['Complete Payment', 'Pending Payment', 'Cancel Payment'];
+  List transctionDataList = [
+    'Transaction ID',
+    'Gateway',
+    'Amount',
+    'Charge',
+    'Status',
+    'Date-Time'
+  ];
 
   String _dropDownValue = "All Payment";
-  TextEditingController _languageController = TextEditingController();
-  TextEditingController _date = TextEditingController();
+  final TextEditingController _languageController = TextEditingController();
+  final TextEditingController _dateController = TextEditingController();
+  DateTime selectedDate = DateTime.now();
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+    if (pickedDate != null && pickedDate != selectedDate) {
+      setState(() {
+        selectedDate = pickedDate;
+        _dateController.text = DateFormat('yyyy-MM-dd').format(selectedDate);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.sizeOf(context).height;
@@ -193,10 +221,21 @@ class _DepositHistoryScreenState extends State<DepositHistoryScreen> {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
         child: Column(
+          //  mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Text(
-              'Deposit History',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            const Padding(
+              padding: EdgeInsets.only(left: 35),
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  'Deposit History',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: height * 0.01,
             ),
             InkWell(
               onTap: () {
@@ -233,11 +272,13 @@ class _DepositHistoryScreenState extends State<DepositHistoryScreen> {
                                       icon: const Icon(Icons.close))
                                 ],
                               ),
-                              Container(
-                                height: height * 0.001,
-                                width: width,
-                                decoration:
-                                    const BoxDecoration(color: Colors.black),
+                              Center(
+                                child: Container(
+                                  height: height * 0.001,
+                                  width: width,
+                                  decoration:
+                                      const BoxDecoration(color: Colors.black),
+                                ),
                               ),
                               SizedBox(
                                 height: height * 0.02,
@@ -249,54 +290,55 @@ class _DepositHistoryScreenState extends State<DepositHistoryScreen> {
                               SizedBox(
                                 height: height * 0.01,
                               ),
-                              Expanded(
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 10),
-                                  child: DropdownButtonFormField<String>(
-                                    value: _selectedLanguage,
-                                    decoration: const InputDecoration(
-                                      border: OutlineInputBorder(),
-                                      contentPadding:
-                                          EdgeInsets.symmetric(horizontal: 10),
-                                    ),
-                                    items: <String>[
-                                      'All Payment',
-                                      'Complete Payment',
-                                      'Pending Payment',
-                                      'Cancel Payment'
-                                    ].map((String value) {
-                                      return DropdownMenuItem<String>(
-                                        value: value,
-                                        child: Text(value),
-                                      );
-                                    }).toList(),
-                                    onChanged: (newValue) {
-                                      setState(() {
-                                        _selectedLanguage = newValue!;
-                                        _languageController.text = newValue;
-                                      });
-                                    },
-                                  ),
-                                ),
-                              ),
-                              TextFormField(
-                                controller: _date,
+                              DropdownButtonFormField<String>(
+                                value: _selectedLanguage,
                                 decoration: const InputDecoration(
-                                    suffixIcon: Icon(Icons.calendar_today),
-                                    labelText: 'Select Date'),
-                                onTap: () async {
-                                  DateTime? pickeddate = await showDatePicker(
-                                    context: context,
-                                    initialDate: DateTime.now(),
-                                    firstDate: DateTime(1980),
-                                    lastDate: DateTime(2024),
+                                  border: OutlineInputBorder(),
+                                  contentPadding:
+                                      EdgeInsets.symmetric(horizontal: 10),
+                                ),
+                                items: <String>[
+                                  'All Payment',
+                                  'Complete Payment',
+                                  'Pending Payment',
+                                  'Cancel Payment'
+                                ].map((String value) {
+                                  return DropdownMenuItem<String>(
+                                    value: value,
+                                    child: Text(value),
                                   );
-                                  // final formateddate = DateFormat('dd-MM-yyyy').formate(pickeddate);
+                                }).toList(),
+                                onChanged: (newValue) {
                                   setState(() {
-                                    // _date.text = formateddate.toString();
+                                    _selectedLanguage = newValue!;
+                                    _languageController.text = newValue;
                                   });
                                 },
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              TextFormField(
+                                readOnly: true,
+                                controller: _dateController,
+                                decoration: const InputDecoration(
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(10))),
+                                  suffixIcon: Icon(Icons.calendar_today),
+                                  labelText: 'Select Date',
+                                ),
+                                onTap: () {
+                                  _selectDate(context);
+                                },
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              appSearchButtonWidget(
+                                height: height * 0.05,
+                                width: width * 0.80,
+                                onPressed: () {},
                               )
                             ]),
                           ),
@@ -304,32 +346,74 @@ class _DepositHistoryScreenState extends State<DepositHistoryScreen> {
                       ));
                     });
               },
-              child: Container(
-                height: height * 0.07,
-                width: width * 0.8,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  gradient: const LinearGradient(
-                      colors: [Color(0xfffaaf40), Color(0xff01acee)]),
+              child: Center(
+                child: Container(
+                  height: height * 0.07,
+                  width: width * 0.8,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    gradient: const LinearGradient(
+                        colors: [Color(0xfffaaf40), Color(0xff01acee)]),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'FILTER',
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                      SizedBox(
+                        width: width * 0.02,
+                      ),
+                      Image.asset(
+                        'assets/filter.png',
+                        height: height * 0.04,
+                        color: Colors.white,
+                      )
+                    ],
+                  ),
                 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+              ),
+            ),
+            SizedBox(
+              height: height * 0.01,
+            ),
+            Expanded(
+              child: Container(
+                // color: Colors.black,
+                decoration:
+                    BoxDecoration(borderRadius: BorderRadius.circular(10)),
+                child: Column(
                   children: [
-                    const Text(
-                      'FILTER',
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
+                    Container(
+                      height: height * 0.08,
+                      width: width * 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.blueAccent,
+                      ),
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        shrinkWrap: true,
+                        itemCount: transctionDataList.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              transctionDataList[index],
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 20),
+                            ),
+                          );
+                        },
+                      ),
                     ),
-                    SizedBox(
-                      width: width * 0.02,
-                    ),
-                    Image.asset(
-                      'assets/filter.png',
-                      height: height * 0.04,
-                      color: Colors.white,
-                    )
+                    Text('no data ')
                   ],
                 ),
               ),
